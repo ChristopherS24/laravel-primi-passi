b498d5ca30ab1bc2365ae0e2e9cdc6f844f489b6
ChristopherS24 <chryskies@gmail.com>
2024-10-15 14:48:07 +0200

    hello world inserito

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+<body>
+    <main>
+        <div>
+            <h1>
+                Laravel
+            </h1>
+        </div>
+
+        <div>
+            <h3>
+                Hello World!
+            </h3>
+        </div>
+    </main>
+    
+</body>
+</html>
